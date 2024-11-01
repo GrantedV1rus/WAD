6395570bebf16efade2109783fc3f0167d92594a
GrantedV1rus <t.s.mungoshi@edu.salford.ac.uk>
2024-11-01 07:03:56 +0000

    1

---
+++ Assets/JS/main.js
@@ -0,0 +1,7 @@
+//About Page OnCLick
+function mission() {
+    alert("Spartans, listen up. You've faced impossible odds before, but this mission will test every limit you've got. Our intel's shaky, Covenant presence is off the charts, and we don't have room for errors. Your objective is clear: secure that forerunner artifact and neutralize any hostiles in your path. Stick together, cover each other's six, and remember—no one else can do what you do. So get in there, make every shot count, and show them why Spartans are humanity’s best hope. Lasky out.");
+}
+
+
+
